e34193cec400da6be6f68993fbaf5deb72688302
ElvineJoy <elvinkuiyungest@gmail.com>
2024-09-04 15:08:50 +0300

    Chatterbox CRUD COMPLETE

--- server/app.py
+++ server/app.py
@@ -14,13 +14,89 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by('created_at').all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username= data['username']
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This message does not exit.Please try again."
+        }
+        response = make_response(response_body, 404)
+        return response
+    
+
+    else:
+        if request.method== 'GET':
+            message_dict = message.to_dict()
+            
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+    
+        elif request.method == 'PATCH':
+            #retrieve the data
+            data = request.get_json()
+            #loop to iterate over the key values
+            for attr in data:
+                setattr(message, attr, data[attr])
+
+            db.session.add(message)
+            db.session.commit()
+
+            response=make_response(
+                jsonify(message.to_dict()),
+                200
+            )
+
+            return response
+        
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response =make_response(
+                jsonify({'delete': True})
+            )
+
+            return response
+
+
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
